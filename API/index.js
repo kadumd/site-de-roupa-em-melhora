121fa4663b09468fe6422ec736f696c9604ffc2e
cadumd <cadu.md2021@gmail.com>
2024-09-05 18:15:03 -0300

    uhuh

--- API/index.js
+++ API/index.js
@@ -7,13 +7,25 @@ const portaDeEntrada = 3000;
 const servidor = http.createServer((pedido, resposta) => {
     console.log(pedido.url)
     if (pedido.url === '/') {
-        fs.readFile('frontend/view/index.html', (erro, dadosDoArquivo) => {
+        fs.readFile('frontend/view/pasta-do-site/inicio/index.html', (erro, dadosDoArquivo) => {
             if (erro) {
                 resposta.writeHead(500, { 'Content-Type': 'text/plain' });
                 resposta.end('Erro no servidor!');
                 return;
             }
             resposta.writeHead(200, { 'Content-Type': 'text/html' });
+            resposta.end(dadosDoArquivo);
+            return
+        })
+    }
+    if (pedido.url === '/frontend/view/pasta-do-site/inicio/main.css') {
+        fs.readFile('frontend/view/pasta-do-site/inicio/main.css', (erro, dadosDoArquivo) => {
+            if (erro) {
+                resposta.writeHead(500, { 'Content-Type': 'text/plain' });
+                resposta.end('Erro no servidor!');
+                return;
+            }
+            resposta.writeHead(200, { 'Content-Type': 'text/css' });
             resposta.end(dadosDoArquivo);
             return
         })
